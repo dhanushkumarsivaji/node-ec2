8d0025e6192c79ccd79e5d5fc96c048c8010f71d
DhanushkumarSivaji <dhanushkumar.s@yahoo.com>
2019-10-12 10:57:58 +0530

    health check endpoint

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const healthCheck = require('express-healthcheck')
 const app = express();
 
 app.use(express.json({ extended: false }));
@@ -6,6 +7,12 @@ app.use(express.json({ extended: false }));
 app.get("/", (req, res, next) => {
     res.send(`<h1>welcome ${req.headers.host}</h1>`);
   });
+
+  app.use('/health', require('express-healthcheck')({
+    healthy: function () {
+        return 'healthy';
+    }
+}));
 
 
   app.listen(5000, () => {
