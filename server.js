0b1c59b9fe3037564571401aeb5b8a3e7c4d1fb1
Dhanushkumar Sivaji <dhanushkumar.sivaji@gswvss.com>
2019-11-06 15:35:47 +0530

    added availability zone functionality v3

--- server.js
+++ server.js
@@ -8,6 +8,8 @@ app.use(express.json({ extended: false }));
 app.get("/", async (req, res, next) => {
 
     const location = await request('http://169.254.169.254/latest/meta-data/placement/availability-zone',function(error,response,body){
+      console.log("response",response)
+      console.log("body",body)
         return body
     })
 
