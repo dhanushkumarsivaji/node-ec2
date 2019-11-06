ab092afe0ebc8b4986060fde443585e911bd1fdc
Dhanushkumar Sivaji <dhanushkumar.sivaji@gswvss.com>
2019-11-06 15:27:08 +0530

    added availability zone functionality

--- server.js
+++ server.js
@@ -1,19 +1,25 @@
 const express = require("express");
 const healthCheck = require('express-healthcheck')
 const app = express();
+var request = require('request')
 
 app.use(express.json({ extended: false }));
 
-app.get("/", (req, res, next) => {
-    console.log(req.connection.remoteAddress,
-      req.connection.remotePort,
-      req.connection.localAddress,
-    )
+app.get("/", async (req, res, next) => {
+
+    const location = await request('http://169.254.169.254/latest/meta-data/placement/availability-zone',function(error,response,body){
+        return response
+    })
+
+    
+    
     res.send(`<h1> Host address : ${req.headers.host}</h1>
               
               <h1> Remote port : ${req.connection.remotePort}</h1>
 
               <h1> Local address : ${req.connection.localAddress}</h1>
+
+              <h1> Region : ${location}</h1>
               `);
   });
 
