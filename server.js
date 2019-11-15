21e613d63e113c83df822beba7b4f5b962dc9929
DhanushkumarSivaji <dhanushkumar.sivaji@gswvss.com>
2019-11-15 17:28:36 +0530

    v1

--- server.js
+++ server.js
@@ -1,18 +1,13 @@
 const express = require("express");
-const healthCheck = require('express-healthcheck')
+
 const app = express();
-var functions = require('./myfunction')
-const axios = require('axios')
+
+
 
 app.use(express.json({ extended: false }));
 
 app.get("/", async (req, res, next) => {
 
-   
-
-    axios.get('http://169.254.169.254/latest/meta-data/placement/availability-zone')
-      .then(res => console.log(res))
-    
     res.send(`<h1> Host address : ${req.headers.host}</h1>
               
               <h1> Remote port : ${req.connection.remotePort}</h1>
